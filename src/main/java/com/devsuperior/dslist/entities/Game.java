0dc19c0517df55c75eb44eb2690ae30709c5e58a
Erick Silva <erick.ap.silva@gmail.com>
2023-08-28 07:54:39 -0300

    game seed

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -16,8 +16,13 @@ public class Game {
     private Integer year;
     private String genre;
     private String platforms;
+    private Double score;
+
     private String imgUrl;
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
     public Game(){
@@ -25,16 +30,25 @@ public class Game {
     }
 
     public Game(Long id, String title, Integer year, String genre,
-                String platforms, String imgUrl, String shortDescription,
+                String platforms, Double score, String imgUrl, String shortDescription,
                 String longDescription) {
         this.id = id;
         this.title = title;
         this.year = year;
         this.genre = genre;
         this.platforms = platforms;
+        this.score = score;
         this.imgUrl = imgUrl;
         this.shortDescription = shortDescription;
         this.longDescription = longDescription;
+    }
+
+    public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
     }
 
     public Long getId() {
